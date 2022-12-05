a802383dcb4c776c4ee19af6100cf9895426f431
René Hartjes <rene.hartjes@q-railing.com>
2022-12-05 15:34:33 +0100

    next commit - footer

--- src/components/includes/Footer.js
+++ src/components/includes/Footer.js
@@ -3,41 +3,145 @@ import './Footer.css'
 
 function Footer(props) {
  
-  let gerwords = [["Über uns","/projects"],["Karriere","/projects"],["Kontakt","/projects"],["Bestellung und Lieferung","/projects"],["Downloads","/projects"],["Neuigkeiten","/projects"]
-  ["Events","/projects"],["Videos","/projects"],["Balkongeländer","/projects"],["Französicher Balkon","/projects"],["Geländer","/projects"],["Glasgeländer","/projects"],["Handlaufhalter","/projects"],["Treppenhandlauf","/projects"],
-  ["Treppengeländer","/projects"],["Q-railing","Marie-Curie-Strasse 8-14","46446 Emmerich am Rhein","Deutschland"],["T. 02822 915 69 0","F. 02822 915 69 70","WhatsApp 0152 08651286","sales.de@q-railing.com"]
-  ,["Q-railing","Hanns-Schwindt-Strasse 8","81829 München","Deutschland"] ,["T. 089 24 887 460","F. 089 24 887 469 9","WhatsApp 0152 08651286","sales.de@q-railing.com"]
-];
-let engwords = [["About us","/project"],["Career","/projects"],["Contact","/projects"],["Ordering and delivery","/projects"],["BIM objects","/projects"],["News","/projects"]
-["Downloads","/projects"],["Videos","/projects"],["Railing","/projects"],["Glass railing","/projects"],["Stair handrail and stair railing","/projects"],["Handrail brackets","/projects"],["Handlaufhalter","/projects"],["Treppenhandlauf","/projects"],
-["Treppengeländer","/projects"],["Q-RAILING CALIFORNIA","14321 Franklin Ave.","Tustin, CA 92780","USA"],["T. +1 714 259 1372","F. +1 714 259 1720","","sales.us@q-railing.com"]
-,["Q-RAILING NEW YORK","2555 Stillwell Avenue","Brooklyn, NY 11223","USA"] ,["T. 089 24 887 460","F. 089 24 887 469 9","WhatsApp 0152 08651286","sales.de@q-railing.com"]
-];
-let [langstate, setlangstate] = useState(engwords);
+
+let [langstate, setlangstate] = useState();
+let [values, setvalues] = useState([]);
+let valarr = [];
+let valarr2 = [];
+let valarr3 = [];
+useEffect(()=>{
+  console.log("prefetch:"+"ger")
+  fetcher()
+
+},[])
+
+
   useEffect(()=>{
-    if(props.language == "GER"){
-      setlangstate(gerwords)
-    }else{
-      setlangstate(engwords)
-    }
+    console.log("prefetch:"+props.language)
+    fetcher()
+
   },[props.language])
+
+  function fetcher(){
+    if(props.language){
+    fetch('https://squid-app-9h43v.ondigitalocean.app/api/texts?filters[systemtitel][$eq]=footer&filters[language][$eq]='+props.language.toLowerCase())
+    .then(res=>res.json())
+    .then(json=>setlangstate(json)).then(console.log(langstate)).then(()=>footerrender())}
+    else{
+      fetch('https://squid-app-9h43v.ondigitalocean.app/api/texts?filters[systemtitel][$eq]=footer&filters[language][$eq]=ger')
+      .then(res=>res.json())
+      .then(json=>setlangstate(json)).then(console.log(langstate)).then(()=>footerrender()).then(()=> footrenderaddr())
+    }
+  }
+
+  function footerrender(){
+
+
+    
+
+    if(langstate){
+      console.log(langstate)
+      let data1 = langstate.data[0].attributes.text1.split("§§")
+      let data2 = langstate.data[0].attributes.text2.split("§§")
+      let data3 = langstate.data[0].attributes.attributes.split("§§")
+
+      for (let i = 0; i < data1.length; i++) {
+        let returnval = data1[i].split("||")
+        valarr.push([returnval[0],returnval[1]])
+      }
+
+      for (let i = 0; i < data2.length; i++) {
+        let returnval = data2[i].split("||")
+        valarr2.push([returnval[0],returnval[1]])
+      }
+
+      for (let i = 0; i < data3.length; i++) {
+        let returnval = data3[i].split("||")
+        valarr3.push([returnval[0],returnval[1]])
+      }
+
+      footrenderaddr()
+      footrenderprods()
+      return(
+        <>
+         
+          <ul id="Footer-block1">
+          {
+     
+     valarr.map((statss, index) => (
+      <li> 
+        <a href={statss[1]}>{statss[0]}</a>
+      </li>
+  
+     ))}
+            </ul>
+
+            
+        </>
+      )
+
+
+
+    }else{
+      return(
+        <>
+ 
+        </>
+      )
+    }
+  }
+
+  function footrenderprods(){
+    console.log("called")
+    console.log(valarr3)
+    return(
+      <ul id="Footer-block2">
+      {
+             valarr2.map((statss, index) => (
+              <li> 
+                <a href={statss[1]}>{statss[0]}</a>
+              </li>
+          
+             ))}
+    </ul>
+    )
+  }
+
+  function footrenderaddr(){
+    return(
+      <ul id="Footer-block3">
+      {
+             valarr3.map((statss, index) => (
+              <li> 
+                <a href={statss[1]}>{statss[0]}</a>
+              </li>
+          
+             ))}
+    </ul>
+    )
+  }
 
   return (
     <div className='Footer-wrap'>
       <div className='Footer-inner'>
-    <div className='Footer-mid'>
-        <h3>Q-RAILING</h3>
-        <div><ul><li>Login</li><li>Mein Konto</li><li>Favoriten</li><li>Kontakt</li><li>Bestellen</li><li>Bezahlen</li><li>Versand</li><li>Mehr Q-railing</li></ul></div>
-        {langstate[1][0]}
-    </div>
-    <br />
-    <br />
+      <img src="https://www.q-railing.com/files/q-railing-logo.jpg" id="footerlogo" width={133} height="35"/>
+      <div className='Footer-top'>
+      
+        <div className='Footer-Links'>
+    
+            {footerrender()}
+            {footrenderprods()}
+           
+            
+        </div>
+        <div className='Footer-Address'>
+        {footrenderaddr()}
+        </div>
+      </div>
+      <div className='Footer-bot'>
 
-    <div className='Footer-bot'>
-        <div><p>Copyright 2022 Q-railing Central Europe GmbH. Alle Rechte Vorbehalten.</p></div>
-        <div><ul><li>Datenschutz</li><li>Impressum</li><li>AGB</li></ul></div>
-    </div>
-    </div>
+      </div>
+      </div>
     </div>
   )
 }
